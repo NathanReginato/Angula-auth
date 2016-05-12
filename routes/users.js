7581d797997937356f8e28c8fe07d3041c07c777
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-12 15:19:07 -0600

    Added me endpoint

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,29 @@ var bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken')
 
 /* GET users listing. */
+
+router.get('/me', function(req, res, next) {
+
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(' ')[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // payload is {id: 56}
+    knex('users').where({id: payload.id}).first().then(function (user) {
+      if (user) {
+        res.json({id: user.id, name: user.name})
+      } else {
+        res.status(403).json({
+          error: "Invalid ID"
+        })
+      }
+    })
+  } else {
+    res.status(403).json({
+      error: "No token"
+    })
+  }
+})
+
 router.post('/signup', function(req, res, next) {
 
   const errors = [];
