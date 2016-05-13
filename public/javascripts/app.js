a2893219d62a6db5eb18cf8d06d331016014975d
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-12 21:40:56 -0600

    Beers route is protected

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,13 +7,78 @@
        .when('/', {
         templateUrl: '/templates/home.html',
         controller: 'HomeController',
+        resolve: {
+          currentUser: function ($http) {
+
+            // if the browser has a token
+            if (localStorage.getItem('token')) {
+
+              // call /me endpoint, and pass it the token
+              const config = {
+                headers: {
+                  'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+              }
+
+              return $http.get('/api/v1/users/me', config)
+                .then(function (response) {
+                  return response.data
+                })
+                .catch(function () {
+                  localStorage.clear();
+                  return null;
+                })
+
+            }
+          }
+        }
       })
       .when('/signup', {
         templateUrl: '/templates/signup.html',
         controller: 'SignupController'
+      })
+      .when('/beers', {
+        templateUrl: '/templates/beers.html',
+        controller: 'BeersController',
+        requiresLogin: true,
+        resolve: {
+          currentUser: function ($http, $location) {
+
+            // if the browser has a token
+            if (localStorage.getItem('token')) {
+
+              // call /me endpoint, and pass it the token
+              const config = {
+                headers: {
+                  'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+              }
+
+              return $http.get('/api/v1/users/me', config)
+                .then(function (response) {
+                  return response.data
+                })
+                .catch(function () {
+                  localStorage.clear();
+                  $location.path('/')
+                  return null;
+                })
+
+            }
+          }
+        }
       });
+
+      // app.run runs once when the app starts
 
       $locationProvider.html5Mode(true);
     });
 
+    angular.module('app').run(function ($rootScope, $location, $window) {
+      $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (next.$$route.requiresLogin && !localStorage.getItem('token')) {
+          $location.path('/');
+        }
+      });
+    });
 }());
--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -2,14 +2,11 @@
   'use strict';
 
   angular.module('app')
-    .controller('SignupController', function ($scope, $http, $window, $location, $rootScope) {
+    .controller('SignupController', function ($scope, $http, $window, $location) {
 
       $scope.createUser = function () {
-        console.log($scope.user);
-
         $http.post('/api/v1/users/signup', $scope.user)
         .then(function(response){
-          $rootScope.user = $scope.user
           $window.localStorage.setItem('token', response.data.token);
           $location.path('/')
         })
